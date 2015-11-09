28a24d0b0b95ec1f15c638d779c682943a0ce5ed
NilsVA <you@example.com>
2015-11-09 16:34:35 +0100

    hw2 NilsVA

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,17 +8,27 @@ int main(){
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+        double d[Nend-Nskip];
+
+	ofstream out("output.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
 	   for(int i=1; i <= Nskip; i++)
 		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
+	   for(int i=1; i <= Nend-Nskip; i++){
 	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
-	}
+	   		   //cout << r << "\t" << x << endl;
+                           d[i]=x;
+                        }
+           for(int j=1; j<= Nend-Nskip;j++){
+   	               
+			out << r << "\t" << d[j] << endl;
+			
+           }
 
+	}
+	out.close();
 
 	return 0;
 }
